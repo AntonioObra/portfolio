40c191f510428f5213b111360d8918fa38fa59dc
Antonio Obradović <antonio.obra8@gmail.com>
2023-12-12 18:29:08 +0100

    Removed - work projects

--- .contentlayer/generated/Project/_index.mjs
+++ .contentlayer/generated/Project/_index.mjs
@@ -2,8 +2,6 @@
 
 import projects__blogMdx from './projects__blog.mdx.json' assert { type: 'json' }
 import projects__coderepoMdx from './projects__coderepo.mdx.json' assert { type: 'json' }
-import projects__mywiipbaMdx from './projects__mywiipba.mdx.json' assert { type: 'json' }
 import projects__portfolioMdx from './projects__portfolio.mdx.json' assert { type: 'json' }
-import projects__wiipEngravingGeneratorMdx from './projects__wiip-engraving-generator.mdx.json' assert { type: 'json' }
 
-export const allProjects = [projects__blogMdx, projects__coderepoMdx, projects__mywiipbaMdx, projects__portfolioMdx, projects__wiipEngravingGeneratorMdx]
+export const allProjects = [projects__blogMdx, projects__coderepoMdx, projects__portfolioMdx]
