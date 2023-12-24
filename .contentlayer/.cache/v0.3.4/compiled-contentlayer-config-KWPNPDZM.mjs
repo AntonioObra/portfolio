eaa46f4c6575da789209ba6c19a4d4978620d8b5
Antonio Obradović <antonio.obra8@gmail.com>
2023-12-24 20:05:55 +0000

    Updated - portfolio text, coderepo image

---
+++ .contentlayer/.cache/v0.3.4/compiled-contentlayer-config-KWPNPDZM.mjs
@@ -0,0 +1,77 @@
+// contentlayer.config.js
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import rehypePrettyCode from "rehype-pretty-code";
+import remarkGfm from "remark-gfm";
+var computedFields = {
+  slug: {
+    type: "string",
+    resolve: (doc) => `\u02D9/${doc._raw.flattenedPath}`
+  },
+  slugAsParams: {
+    type: "string",
+    resolve: (doc) => doc._raw.flattenedPath.split("/").slice(1).join("/")
+  }
+};
+var Project = defineDocumentType(() => ({
+  name: "Project",
+  filePathPattern: `projects/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true
+    },
+    description: {
+      type: "string",
+      required: true
+    },
+    github: {
+      type: "string",
+      required: true
+    },
+    url: {
+      type: "string",
+      required: true
+    },
+    tag: {
+      type: "string",
+      required: true
+    },
+    image: {
+      type: "string",
+      required: true
+    }
+  },
+  computedFields
+}));
+var contentlayer_config_default = makeSource({
+  contentDirPath: "src/content",
+  documentTypes: [Project],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      [
+        rehypePrettyCode,
+        {
+          theme: "github-dark",
+          onVisitLine(node) {
+            if (node.children.length === 0) {
+              node.children = [{ type: "text", value: " " }];
+            }
+          },
+          onVisitHighlightedLine(node) {
+            node.properties.className.push("line--highlighted");
+          },
+          onVisitHighlightedWord(node) {
+            node.properties.className.push("line--highlighted");
+          }
+        }
+      ]
+    ]
+  }
+});
+export {
+  Project,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-KWPNPDZM.mjs.map
