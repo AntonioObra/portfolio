17a08e8978c9241bdfd9e326dee1c35e31cac984
AntonioObra <antonio@obradovic.dev>
2025-04-15 23:28:49 +0200

    added: new website 2025

--- src/types/stack.d.ts
+++
@@ -1,9 +0,0 @@
-import { Icon } from "@/components/Icons";
-
-export type Stack = {
-  name: string;
-  icon: Icon;
-  description: string;
-  shortDescription: string;
-  tag: "Frontend" | "Backend" | "Database" | "DevOps" | "Other";
-};
